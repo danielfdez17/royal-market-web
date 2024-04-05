631a2d676727934f48cf1ef3d30bdf87eddcd972
danielfdez17 <daniel17dev@gmail.com>
2024-04-05 16:43:45 +0200

    dependency installed and pool.js converted to ecmascript

--- database/pool.js
+++ database/pool.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
-const mysql = require("mysql");
+import { createPool } from 'mysql'
+require('dotenv').config()
 
-const pool = mysql.createPool({
+const pool = createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
+  port: process.env.DB_PORT
+})
 
-module.exports = pool;
+export default pool
