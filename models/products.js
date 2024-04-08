167a42d98e4a7539de62158a8b0f4f1c99d7fc36
danielfdez17 <daniel17dev@gmail.com>
2024-04-08 17:13:10 +0200

    Dependencies added

--- models/products.js
+++ models/products.js
@@ -1,3 +1,10 @@
+'use strict'
+const pool = require('../db/pool.js')
+const DAOProducts = require('../db/DAOProducts.js')
+const { validateProduct, validatePartialProduct } = require('../schemas/products.js')
+
+const daoProducts = new DAOProducts(pool)
+
 export class ProductsModel {
   static async create ({ input }) {}
   static async getById ({ id }) {}
