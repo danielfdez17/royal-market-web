8e1a825a7bc705cbaa050cc5aedaf76b084f8834
danielfdez17 <daniel17dev@gmail.com>
2024-05-25 08:42:00 +0200

    test prepare to be coded up

---
+++ test/spec/daoProducts.test.js
@@ -0,0 +1,17 @@
+// const DAOProducts = require("../../db/DAOProducts.js");
+
+describe("Test DAOProducts", () => {
+  it("Create product success", async () => {});
+  it("Create product error, invalid syntax", async () => {});
+  it("Create product error, product already active", async () => {});
+  it("Create product error, product is inactive", async () => {});
+  it("Get product by id success", async () => {});
+  it("Get product by id error, product not found", async () => {});
+  it("Get all products success", async () => {});
+  it("Update product success", async () => {});
+  it("Update product error, invalid syntax", async () => {});
+  it("Update product error, product not found", async () => {});
+  it("Delete product success", async () => {});
+  it("Delete product error, product not found", async () => {});
+  it("Delete product error, product is already inactive", async () => {});
+});
