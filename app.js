423d593fda2c3f68c4e84e48125a27c44bb3f6eb
danielfdez17 <daniel17dev@gmail.com>
2024-10-27 17:00:53 +0100

    routers named updated

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ module.exports = {
 
 const PORT = process.env.PORT || 3001;
 
-const indexRouter = require("./routes/indexRouter.js");
-// const warehousesRouter = require('./routes/users.js')
-// const clientsRouter = require('./routes/clients.js')
-// const salesRouter = require('./routes/sales.js')
-// const productsRouter = require('./routes/products.js')
-// const providersRouter = require('./routes/providers.js')
-// const workersRouter = require('./routes/workers.js')
+const indexRouter = require("./routes/index.js");
+const clientsRouter = require('./routes/clients.js')
+const productsRouter = require('./routes/products.js')
+const providersRouter = require('./routes/providers.js')
+const salesRouter = require('./routes/sales.js')
+const warehousesRouter = require('./routes/warehouses.js')
+const workersRouter = require('./routes/workers.js')
 
 const app = express();
 
@@ -28,12 +28,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-// app.use('/warehouses', warehousesRouter)
-// app.use('/clients', clientsRouter)
-// app.use('/sales', salesRouter)
-// app.use('/products', productsRouter)
-// app.use('/providers', providersRouter)
-// app.use('/workers', workersRouter)
+app.use('/clients', clientsRouter)
+app.use('/products', productsRouter)
+app.use('/providers', providersRouter)
+app.use('/sales', salesRouter)
+app.use('/warehouses', warehousesRouter)
+app.use('/workers', workersRouter)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
