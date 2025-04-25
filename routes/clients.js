dd157ba455666a919f88b4b1de1331a3a31c5021
danielfdez17 <daniel17dev@gmail.com>
2025-04-25 20:00:52 +0200

    FEAT: main branch cleaned

--- routes/clients.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  res.render("clients");
-});
-
-module.exports = router;
