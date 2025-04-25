7332cbdcecbb10c2a4da06dca345ff0bee3132fa
danielfdez17 <daniel17dev@gmail.com>
2025-04-25 20:08:29 +0200

    FEAT: initial commit

---
+++ src/test/java/java/practice/royalmarket/RoyalmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package java.practice.royalmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
